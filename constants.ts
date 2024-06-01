19466f02a97ccc7db79b89cd148d2bb0ccf99aa1
yuichkun <yuichkun@gmail.com>
2024-06-01 18:20:21 +0900

    replicate tmp files

--- constants.ts
+++ constants.ts
@@ -1,8 +1,16 @@
-export const WORKS = [
+const _WORKS = [
   // tmp
   {
-    name: "Chair",
+    name: "Astronaut",
     url: "https://modelviewer.dev/shared-assets/models/Astronaut.glb",
+    slug: "astronaut",
+  },
+  {
+    name: "Chair",
+    url: "/chair.glb",
     slug: "chair",
   },
 ] satisfies App.Work[];
+
+// copy _WORKS 100 times
+export const WORKS = Array.from({ length: 100 }, () => _WORKS).flat();
