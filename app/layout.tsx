bcfd7337e3bb7d8e9229f3c0180694f4331756db
yuichkun <yuichkun@gmail.com>
2024-06-01 16:58:44 +0900

    tmp top page style

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Binge's Official Site",
+  title: "BINGE's Official Site",
   description: "3D Artist Binge's Official Site",
 };
 
