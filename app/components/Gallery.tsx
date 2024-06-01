335696955568a953b02a6e7861e67f50881e73b3
yuichkun <yuichkun@gmail.com>
2024-06-01 16:38:11 +0900

    make gallery bigger

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -5,9 +5,10 @@ type Props = App.Work;
 
 export function Gallery({ name, url }: Props) {
   return (
-    <div>
+    <div className="relative">
       <h1>{name}</h1>
       <model-viewer
+        style={{ width: "100vw", height: "100dvh" }}
         src={url}
         alt="A 3D model of an astronaut"
         auto-rotate
