1c7ef10817daa9f793942de3b5d33801756e9e44
yuichkun <yuichkun@gmail.com>
2024-06-01 16:42:47 +0900

    adjust styles

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -6,7 +6,9 @@ type Props = App.Work;
 export function Gallery({ name, url }: Props) {
   return (
     <div className="relative">
-      <h1>{name}</h1>
+      <h1 className="absolute font-bold text-xl mt-4 ml-4 md:text-8xl">
+        {name}
+      </h1>
       <model-viewer
         style={{ width: "100vw", height: "100dvh" }}
         src={url}
